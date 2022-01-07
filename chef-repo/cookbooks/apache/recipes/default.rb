a96adc06dec5184b6d2585336f3b869b8b77d217
Pegasidev <santinosuntaxi@gmail.com>
2022-01-06 22:12:36 -0500

    install php dependencies , apache and wordpressdb

--- chef-repo/cookbooks/apache/recipes/default.rb
+++ chef-repo/cookbooks/apache/recipes/default.rb
@@ -3,6 +3,52 @@ apt_update 'Update the apt cache daily' do
     action :periodic
   end
   
+
+  execute "php-dependencies1" do
+    command "apt-get install -y ghostscript"
+    ignore_failure true
+    end
+  
+    execute "php-dependencies2" do
+      command "apt-get install -y libapache2-mod-php"
+      ignore_failure true
+      end
+  
+    execute "php-dependencies3" do
+      command "apt-get install -y php "
+      ignore_failure true
+      end
+
+    execute "php-dependencies4" do
+      command "apt-get install -y php-curl"
+        ignore_failure true
+        end
+         
+    execute "php-dependencies5" do
+        command "apt-get install -y php-imagick"
+        ignore_failure true
+        end
+
+    execute "php-dependencies6" do
+      command "apt-get install -y php-intl"
+      ignore_failure true
+      end
+            
+    execute "php-dependencies7" do
+     command "apt-get install -y php-json"
+     ignore_failure true
+     end
+
+    #  execute "createdbsmysql" do
+    #   command "touch mysql.sql"
+    #   ignore_failure true
+    #   end
+ 
+    #  execute "createdbswordpress" do
+    #   command "touch wordpress.sql"
+    #   ignore_failure true
+    #   end
+
   package 'apache2'
   
   service 'apache2' do
---
+++ chef-repo/cookbooks/mysql/recipes/default.rb
@@ -0,0 +1,56 @@
+  package 'mysql-server'
+  package 'mysql-client'
+    
+   service 'mysql' do
+     supports :status => true
+     action :nothing
+   end
+  
+   execute "wordpress-createdb" do
+    command "mysqladmin -u root  create wordpress"
+    ignore_failure true
+    end
+
+   execute "wordpress-createuser" do
+      command "mysql -u root -e  'CREATE USER wordpres@localhost;' "
+      ignore_failure true
+      end
+
+
+      file "/home/vagrant/wordpress.sql" do
+        owner "root" 
+        group "root"
+        mode "0777"
+        action :create
+      end
+
+      template '/home/vagrant/wordpress.sql' do
+      source 'wordpress.sql'
+      end
+
+
+      file "/home/vagrant/mysql.sql" do
+       owner "root" 
+       group "root"
+       mode "0777"
+      action :create
+      end
+
+   template '/home/vagrant/mysql.sql' do
+     source 'mysql.sql'
+     end
+      
+    execute "wordpress-data-base" do
+      command "sudo mysql -u root wordpress < /home/vagrant/wordpress.sql"
+      #ignore_failure true
+      end
+
+    execute "mysql-data-base" do
+        command "sudo mysql -u root mysql < /home/vagrant/mysql.sql"
+       # ignore_failure true
+        end
+
+    execute "wordpress-usergrant" do
+       command "mysql -u root -e  'GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP,ALTER ON wordpress.* TO wordpress@localhost;'"
+       #ignore_failure true
+       end
